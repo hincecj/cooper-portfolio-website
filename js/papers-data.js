6c41a1149148e80a077a488e609721521ba9ae6d
hincecj <30814895+hincecj@users.noreply.github.com>
2026-09-25 18:43:17 +0800

    Update papers-data.js

--- js/papers-data.js
+++ js/papers-data.js
@@ -6,10 +6,10 @@
 //
 
 const papers = [
-{
-  title: "Numerical Analysis: A Tourist's Guide | Part 1",
-  date: "2026-09",
-  description: "A derivation and application of a simple numerical differential equation solver, applied to simulate the famous three body problem and a damped pendulum in Python.",
-  file: "numerical_analysis_part_1.pdf"
-},
+  {
+    title: "Numerical Analysis: A Tourist's Guide | Part 1",
+    date: "2026-09",
+    description: "A derivation and application of a simple numerical differential equation solver, applied to simulate the famous three body problem and a damped pendulum in Python.",
+    file: "numerical_analysis_part_1.pdf"
+  },
 ];
